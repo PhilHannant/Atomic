a7f424a0aa2ee70fe11989cb8f360c2991099dfa
philhannant <philip.a.hannant@gmail.com>
2018-01-06 22:15:29 +0000

    doing reflection exercises

---
+++ src/ALittleReflection2.scala
@@ -0,0 +1,21 @@
+import reflect.runtime.currentMirror
+
+object Name {
+  def className(o:Any) =
+    currentMirror.reflect(o).symbol.
+      toString.replace('$', ' ').split(' ').last
+}
+
+trait Name {
+  override def toString = Name.className(this)
+}
+
+
+case class caseClass(name:String)
+
+object printer extends App {
+  println("hello")
+  println(caseClass)
+}
+
+
