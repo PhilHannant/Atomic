22dec0026ae7972fa1cd047a9f78ed8226d888fc
philhannant <philip.a.hannant@gmail.com>
2018-01-06 22:50:36 +0000

    exercise 1 complete

--- src/Polymorphism.scala
+++ src/Polymorphism.scala
@@ -8,9 +8,19 @@ trait Mobile {
 }
 
 class Vechile() extends Mobile {
-  override def moves: String = "vrooom" + super.moves
+  override def moves: String = "vrooom " + super.moves
 }
 
 class Animals() extends Mobile {
-  override def moves: String = "breathing" + super.moves
+  override def moves: String = "breathing " + super.moves
+}
+
+
+object runner extends App {
+  val v = new Vechile
+  val a = new Animals
+  println(v.moves)
+  println(a.moves)
+
+
 }
